43b4dfee522da9143c992cd792e0593e1367ea34
tiger <764427188@qq.com>
2019-04-19 14:49:47 +0800

    restart

--- src/components/D3ForceChart/utils/checkNodesDiff.js
+++
@@ -1,3 +0,0 @@
-export const cheackNodesDiff = (nodes, newNodes) => {
-    return nodes.length > newNodes.length ? 'enter' : 'exit';
-  }
--- src/index.js
+++
@@ -1,8 +0,0 @@
-// export default D3ForceChart;
-import React from 'react';
-import ReactDOM from 'react-dom';
-import D3ForceChart from './components/D3ForceChart/index';
-
-ReactDOM.render(
-<D3ForceChart />, 
-document.getElementById('root'));
