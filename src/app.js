2234c464498315f58bed8004e5e8ea01fd409489
tiger <764427188@qq.com>
2019-04-19 14:54:01 +0800

    d3力导向图

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import D3ForceChart from './D3ForceChart/index';
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <D3ForceChart />
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const isDebug = process.env.NODE_ENV === 'development';
+
+const config = {
+  entry: './src/index',
+  mode: isDebug ? 'development' : 'production',
+  devtool:'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          babelrc: false,
+          presets: [
+            'react',
+            'stage-2'
+          ],
+          plugins: [
+            'transform-class-properties',
+          ]
+        },
+        include: path.join(__dirname, 'src')
+      },
+      {test: /\.css$/, loaders: ['style-loader', 'css-loader']},
+      {test: /\.less/, loaders: ['style-loader', 'css-loader', 'less-loader']}
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+        {from:'./src/D3ForceChart/mock',to:'./lib/mock'}
+    ]),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      hash: false,
+      title: 'd3-force-chart',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
+
+
+if (isDebug) {
+  config.module.rules[0].query.presets.unshift('react-hmre')
+}
+
+module.exports = config;
